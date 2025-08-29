6b38350c42261b3a9338c2d89421439448e17dbf
JhSebastian <jvelandiavelandia677@gmail.com>
2025-08-29 16:54:05 -0500

    Cambios consumo template  estado

---
+++ src/app/components/modals/modal-status/modal-status.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-status',
+  templateUrl: './modal-status.component.html',
+  styleUrls: ['./modal-status.component.scss']
+})
+export class ModalStatusComponent {
+
+}
